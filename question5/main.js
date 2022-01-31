b4e7ca1dfe7595ab73191f5748025d2e6f04dd5a
amverma-systango <amverma@isystango.com>
2022-01-31 16:03:24 +0530

    question 5 corrected

--- question5/main.js
+++ question5/main.js
@@ -1,13 +1,13 @@
 
-let actualNumber = document.getElementById("inp").value;
-// let inp = document.getElementById("inp");
+let actualvalue = document.getElementById("actualvalue");
+let percentage = document.getElementById("percentage");
+
 
 let result = document.getElementById("result");
 
 
+function calculate(){
+	result.innerText = parseFloat(actualvalue.value)*parseFloat(percentage.value)/100; 
 
-function check(){
-	
-	result.innerText = actualNumber;
-
+	console.log(actualvalue+" "+percentage);
 }
