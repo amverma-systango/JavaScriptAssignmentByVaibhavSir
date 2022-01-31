bd2d05ca2c35e19aa7b42d3c73d41f503374592b
amverma-systango <amverma@isystango.com>
2022-01-31 11:32:21 +0530

    question 12 added

--- question9/main.js
+++ question9/main.js
@@ -21,3 +21,30 @@ function check(){
 	}
 	
 }
+
+
+/*
+
+let inp = document.getElementById("inp");
+let result = document.getElementById("result");
+
+function evenOdd( num ){
+
+	if( num === 0 ){
+		return "even";
+	}
+	else if( num === 1){
+		return "odd";
+	}
+	else{
+		return evenOdd(num-2);
+	}
+
+}
+
+function check(){
+	let rows = parseInt(inp.value);
+	result.innerHTML = evenOdd(parseInt(inp.value));
+}
+
+*/
