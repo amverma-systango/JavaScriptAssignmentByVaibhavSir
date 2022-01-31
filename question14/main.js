b78c7f140eb31e29867b06b871e61fffec4e88c1
amverma-systango <amverma@isystango.com>
2022-01-31 18:58:35 +0530

    question 14 corrected

--- question14/main.js
+++ question14/main.js
@@ -2,15 +2,39 @@
 let inp = document.getElementById("inp");
 let result = document.getElementById("result");
 
+let requestPort = ["https://www.","http://www."];
+let requestDomain = [".com",".ru",".org",".net",".ir",".in",".uk",".au",".de",".ua",".gov",".io",".edu",".info",".biz"] 
 
 function check(){
-	let res = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(inp.value);
+	
+	let validPort = false;
+	let validDomain = false;
 
-	console.log(res);
-	if(res){
-		result.innerText = "Yes, its valid URL";
+	
+	for( let x of requestPort){
+		if(inp.value.includes(x)){
+			validPort = true;
+		}
+	}
+	console.log( validPort );
+
+
+
+	for( let x of requestDomain){
+		if(inp.value.includes(x)){
+			validDomain = true;
+		}
+	}
+	console.log( validDomain );
+
+	
+	let res = validPort && validDomain;
+	
+	if(res === true){
+		result.innerText = "Yes, its a valid URL";
 	}
 	else{
-		result.innerText = "No, its not valid URL";
+		result.innerText = "No, its not a valid URL";
+
 	}
 }
